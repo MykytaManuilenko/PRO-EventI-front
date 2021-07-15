7f6a31022069d44d7bd79fb97d67bc2aa3c8f9ab
anastasiia-koval <78612381+anastasiia-koval@users.noreply.github.com>
2021-07-15 23:44:21 +0200

    Delete eventl directory

--- eventl/src/components/UI/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './Button.scss'
-const Button = (props) => (
-    <button className={["ButtonStyle", props.class].join(' ')} onClick={props.clicked}>{props.children}</button>
-);
-
-export default Button;
